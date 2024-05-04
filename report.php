c2b3d7c28383e8eab4d1de12dd99abee8bf539d2
LAB11_USER ON WS16 <L11Y14W16@citulab.net>
2024-05-04 13:44:29 +0800

    .

--- report.php
+++ report.php
@@ -98,6 +98,37 @@ while ($row = $result1->fetch_assoc()) {
 echo "</table>";
 
 
+//NEW TABLE on Subscription Plans
+
+// Query to retrieve all user details
+$query = "SELECT * FROM tblcoffee";
+
+// Execute the query
+$result = $connection->query($query);
+
+// Check if the query was successful
+if (!$result1) {
+    echo "Query failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    exit();
+}
+
+// Create a table to display the user details
+echo "<h1> Coffee Beans </h1>";
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Coffee ID</th><th>Coffee Name</th><th>Coffee Description</th></tr>";
+
+// Loop through the result set and display each user's details
+while ($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>" . $row['Coffee_ID'] . "</td>";
+    echo "<td>" . $row['Coffee_Name'] . "</td>";
+    echo "<td>" . $row['Description'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+
+
 echo "</body></html>";
 
 // Close the database connection
